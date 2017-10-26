7534f0885cc08c76deb7e0d1963c96c2768c722b
Mikko Forsström <pekkisx@gmail.com>
2017-10-26 08:50:19 +0300

    fuubar.

--- client/src/client.js
+++ client/src/client.js
@@ -1,20 +1,8 @@
-/* global document, __DEVELOPMENT__, window */
-/* eslint global-require: "off" */
-
-/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import Redbox from 'redbox-react';
-*/
-
 import { createStore } from './utils/redux';
 import { getMiddlewares, getReducers, getEnhancers, getInitialState } from './config/redux';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './Root';
-
 
 const initialState = getInitialState();
 const store = createStore(
@@ -25,7 +13,6 @@ const store = createStore(
 );
 
 function render(Component, rootElement) {
-
   ReactDOM.render(
     <Component store={store} />,
     rootElement
