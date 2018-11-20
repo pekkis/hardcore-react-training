40b51cfebdd972e09a05f5639c39a97e39a416da
Mikko Forsström <pekkisx@gmail.com>
2018-11-20 17:34:23 +0200

    Added Pose and GraphQL optional stuff.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,9 +3,8 @@ import React from "react";
 const App = props => {
   return (
     <div>
-      <h1>Hardcore React Training!</h1>
-
-      <p>Hellurei!</p>
+      <h1>Hello React Training!</h1>
+      <p>Dear sir or madam, you must be suckling on a duckling!</p>
     </div>
   );
 };
