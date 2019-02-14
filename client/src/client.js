fcafda1490343eb5a1ba8a5c4c6d0782a090847f
Mikko Forsström <pekkisx@gmail.com>
2019-02-14 11:04:08 +0200

    sdisdoids

--- client/src/client.js
+++ client/src/client.js
@@ -14,7 +14,7 @@ import { getInitialState } from "./config/state";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-import rootSaga from "./sagas/root";
+// import rootSaga from "./sagas/root";
 
 library.add(faSpinner);
 
