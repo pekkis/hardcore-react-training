6956dcfb6cccf3af32154eebc207600d57f34867
Mikko Forsström <pekkisx@gmail.com>
2019-08-31 15:27:20 +0300

    Lubranoff.

--- server/src/jwt-auth.js
+++ server/src/jwt-auth.js
@@ -1,10 +1,18 @@
-const jwt = require("express-jwt");
+import jwt from "express-jwt";
 
 const isRevokedCallback = async (req, payload, done) => {
   return done(null, false);
 };
 
-export default jwt({
-  secret: process.env.SECRET,
-  isRevoked: isRevokedCallback
-});
+export default requireAuth => {
+  if (!requireAuth) {
+    return (req, res, next) => {
+      return next();
+    };
+  }
+
+  return jwt({
+    secret: process.env.SECRET,
+    isRevoked: isRevokedCallback
+  });
+};
