95c63e5a41ff8a3b12d177c284c2a2590a1ef681
Mikko Forsström <pekkisx@gmail.com>
2019-08-31 22:00:43 +0300

    Liub.

--- client/src/components/notifications/Notifications.js
+++ client/src/components/notifications/Notifications.js
@@ -2,21 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Notification from "./Notification";
 
-/*
-The old HOC way of connecting here
-
-import { connect } from "react-redux";
-import Notifications from "../Notifications";
-import { dismissNotification } from "../../../ducks/notification";
-
-export default connect(
-  state => ({
-    notifications: state.notification.get("notifications")
-  }),
-  { dismissNotification }
-)(Notifications);
-*/
-
 const Notifications = props => {
   const { className, notifications, dismissNotification } = props;
 
