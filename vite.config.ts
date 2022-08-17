0045c00d4eec3def174626ad28f0687206b5ca4a
Mikko Forsström <mikko.forsstrom@yle.fi>
2022-08-17 14:41:58 +0300

    Hip hap huu

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic"
+    }),
+    vanillaExtractPlugin()
+  ],
   test: {
     environment: "jsdom",
     deps: {
