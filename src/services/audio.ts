0745463af436d12d028b6c78d55ce7e470aaf177
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2023-11-13 09:00:38 +0200

    Quak kvaak

--- src/services/audio.ts
+++ src/services/audio.ts
@@ -3,12 +3,15 @@ import { random } from "./random";
 export type QuackCastAudio = {
   title: string;
   url: string;
+  description: string;
   attribution: string;
 };
 
 const quackCast: QuackCastAudio = {
   title: "Kvaakcast",
   url: "https://hardcore-react-training.s3.eu-north-1.amazonaws.com/kvaakcast.ogg",
+  description:
+    "Päivän kvaakcastissa toimitusjohtajat **Gaylord McAnkka**, **Kulta-Into Pii** ja **Kroisos Pennonen** keskustelevat talouden ennakoimattomista tapahtumista, niin kutsutuista mustista joutsenista.",
   attribution:
     "https://en.wikipedia.org/wiki/File:Mallard_(Anas_platyrhynchos)_(W1CDR0001518_BD17).ogg"
 };
--- src/services/video.ts
+++ src/services/video.ts
@@ -4,17 +4,21 @@ export type QuackTubeVideo = {
   title: string;
   url: string;
   attribution: string;
+  description: string;
 };
 
 export const videos: QuackTubeVideo[] = [
   {
-    title: "Musta joutsen",
+    title: "Mustat joutsenet",
+    description:
+      "Taloustilanteen ennakoimattomat tapahtumat huolettavat monia. Toimittajamme kysyi mustilta joutsenilta, mitä he *itse* ajattelevat nykyisestä taloustilanteesta ja omasta vaikutuksestaan vuoden 2024 koronnostoihin Euroopan keskuspankissa.",
     url: "https://hardcore-react-training.s3.eu-north-1.amazonaws.com/black-swan.webm",
     attribution:
       "The Nature Box, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
   },
   {
-    title: "Quaak Quaak! Qaak!",
+    title: "Kvauppalehden työharjoittelijat",
+    description: "Kvauppalehden työharjoittelijat esittelyssä",
     url: "https://hardcore-react-training.s3.eu-north-1.amazonaws.com/mallard.webm",
     attribution:
       "https://en.wikipedia.org/wiki/File:Canard_colvert_femelle_et_ses_petits_(Lac_Archambault,_Qc).webm"
