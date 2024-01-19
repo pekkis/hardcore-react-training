d874f84a399768b45747a1274e7d7f4012c2c5b3
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-01-19 10:45:14 +0200

    New service version

--- src/services/video.ts
+++ src/services/video.ts
@@ -1,37 +1,15 @@
-import { random } from "./random";
+import axios from "axios";
+import { getBaseUrl } from "./instance";
 
 export type QuackTubeVideo = {
   title: string;
   url: string;
   attribution: string;
   description: string;
+  type: string;
 };
 
-export const videos: QuackTubeVideo[] = [
-  {
-    title: "Mustat joutsenet",
-    description:
-      "Taloustilanteen ennakoimattomat tapahtumat huolettavat monia. Toimittajamme kysyi mustilta joutsenilta, mitä he *itse* ajattelevat nykyisestä taloustilanteesta ja omasta vaikutuksestaan vuoden 2024 koronnostoihin Euroopan keskuspankissa.",
-    url: "https://hardcore-react-training.s3.eu-north-1.amazonaws.com/black-swan.webm",
-    attribution:
-      "The Nature Box, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
-  },
-  {
-    title: "Kvauppalehden työharjoittelijat",
-    description: "Kvauppalehden työharjoittelijat esittelyssä",
-    url: "https://hardcore-react-training.s3.eu-north-1.amazonaws.com/mallard.webm",
-    attribution:
-      "https://en.wikipedia.org/wiki/File:Canard_colvert_femelle_et_ses_petits_(Lac_Archambault,_Qc).webm"
-  }
-];
-
 export const getVideos = async (): Promise<QuackTubeVideo[]> => {
-  return new Promise((resolve) => {
-    setTimeout(
-      () => {
-        resolve(videos);
-      },
-      random.integer(100, 300)
-    );
-  });
+  const ret = await axios.get<QuackTubeVideo[]>(`${getBaseUrl()}/video`);
+  return ret.data;
 };
