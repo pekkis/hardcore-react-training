a1ca0e240370321a8ec997be0d822fa15fe7bd2c
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-05-19 20:11:47 +0300

    Lussuti lussuti luu, upgrade packages

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,7 +5,6 @@ import "normalize.css";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
